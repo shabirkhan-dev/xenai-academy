7f88fb67d6423bcbbdcbeed1efa496276a8752a3
shabirkhan-dev <135199362+shabirkhan10@users.noreply.github.com>
2024-11-25 00:10:25 +0500

    feat: add shadcn ui

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <div className="font-bold container mx-auto mt-5">
+  <Button className="ring-1 ring-border">Click me</Button>
+  </div>;
+}
